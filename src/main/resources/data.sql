672399a3c60f2f3b11484bc2f87405c9059c0ece
edneyRoldao <edneyroldao@gmail.com>
2020-09-05 10:50:27 -0300

    adicionar estrutura inicial do projeto e script do banco de dados

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-DROP TABLE IF EXISTS document_key;
+DROP TABLE IF EXISTS documento_chave;
 
-CREATE TABLE document_key (
+CREATE TABLE documento_chave (
     id          INT AUTO_INCREMENT  PRIMARY KEY,
-    company_id  INT(6)      NOT NULL,
-    key_value   INT(44)     NOT NULL,
+    id_empresa  INT(6)      NOT NULL,
+    valor_chave INT(44)     NOT NULL,
     status      VARCHAR(10) NOT NULL
 );
